eb4ae02ef078f2c651a5aef560ee558655b7c0cd
sheikh <skuddin.ius@gmail.com>
2022-12-21 01:40:14 +0600

    js data types

---
+++ Data types/assets/js/script.js
@@ -0,0 +1,24 @@
+// Arithmetic Operators(+,-,*,/,**,++,--)
+// let a = 10;
+// let b = 20;
+// let x = a + b;
+// console.log(x);
+
+// Assignment Operators(=,+=,-=,*=,/=,%=,**=)
+// let a = 2;
+// // a += 5;
+// // a -= 5;
+// // a **= 5;
+// concatenate
+// let Name = 'Sheikh';
+// Name += ' Salah Uddin';
+// console.log(Name);
+// console.log(a);
+
+// Comparison Operators(==,===,!=,!==,>,<,>=,<=,?)
+
+// Logical Operators(&&,||,!)
+
+// Conditional Operators
+
+// Type Operators(typeof)
